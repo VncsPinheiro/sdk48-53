550626922fa17e91983977781e3f7a3bf137498d
Vinícius Pinheiro <148554639+VncsPinheiro@users.noreply.github.com>
2025-06-29 16:11:03 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,13 +10,7 @@ import {getFirestore, collection} from 'firebase/firestore'
 // 3. create a web app and copy the firebseConfigs below 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_eXBVz9mSTSJBkRNzco5yH_mLnJ6wICY",
-  authDomain: "teste-f9859.firebaseapp.com",
-  projectId: "teste-f9859",
-  storageBucket: "teste-f9859.firebasestorage.app",
-  messagingSenderId: "510341554746",
-  appId: "1:510341554746:web:a745ee6fa0fb30d3f0cb03",
-  measurementId: "G-VQG2S9R8FX"
+//your data
 };
 
 // Initialize Firebase
